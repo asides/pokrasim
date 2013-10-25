f7e7fff94ed125a3b598185cb811fc98929b4e6c
asides <yaroslav.str@gmail.com>
2013-10-25 14:05:44 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pokrasim::Application.config.secret_token = 'c2488e8fb896481b7336b3d61a7edf56dfc7589ac5b6c905b29c1374e1e1a7a051d0ed9039c0a470aac37170c890eb8655cf391fb0a081d8d410ee4b6358eeb4'
